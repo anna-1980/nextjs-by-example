f368f7d57f5df6d01a66bb89e2c95cd627956749
Anna Zdyb <anna.zdyb@gmail.com>
2023-09-19 14:56:46 +0200

    testing scss with Tailwind, fun :)

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ export default function Home(): JSX.Element {
     <div>
       <h1>Indie Games</h1>
       <p>Only the ebst Indie Games</p>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
