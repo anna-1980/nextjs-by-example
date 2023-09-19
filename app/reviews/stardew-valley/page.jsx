780c1516890bf425570f991d85dd36eab5bf3700
Anna Zdyb <anna.zdyb@gmail.com>
2023-09-19 09:54:08 +0200

    testing routing

--- app/reviews/stardew-valley/page.jsx
+++ app/reviews/stardew-valley/page.jsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 export default function StarDewValleyPage() {
   return (
     <div>
       <h1>StarDew Valley</h1>
       <p>StarDew Valley is a game about farming and making friends.</p>
+      <Link href={"/"}>Home</Link>
     </div>
   );
 }
--- app/sandbox/page.tsx
+++ app/sandbox/page.tsx
@@ -1,7 +1,26 @@
+import Link from "next/link";
+
 export default function Sandbox() {
   return (
     <>
       <h2>Sandbox</h2>
+      <ul>
+        <li>
+          {" "}
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link href={"/reviews"}>Reviews</Link>
+          <ul>
+            <li>
+              <Link href={"/reviews/stardew-valley"}>Star Dew Valley</Link>
+            </li>
+            <li>
+              <Link href={"/reviews/hollow-knights"}>Hollow Knights</Link>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </>
   );
 }
