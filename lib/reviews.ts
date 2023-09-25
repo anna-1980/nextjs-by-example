a6133ec5c36028ea3e1397f3a84db04dc19e5b36
Anna Zdyb <anna.zdyb@gmail.com>
2023-09-25 11:11:42 +0200

    error handle if no file for review

--- lib/reviews.ts
+++ lib/reviews.ts
@@ -10,34 +10,37 @@ export interface Review {
   slug: string;
 }
 
-
-export async function getFeaturedReview () {
-    const reviews = await getReviews();
-    return reviews[0];
-};
+export async function getFeaturedReview() {
+  const reviews = await getReviews();
+  return reviews[0];
+}
 
 export async function getReview(slug: string): Promise<Review> {
-  const textContentMD = await readFile(`./content/reviews/${slug}.md`, "utf-8");
-  const {
-    data: { title, date, image },
-    content,
-  } = matter(textContentMD);
-  const body = marked(content);
-
-  return { slug, title, date, image, body };
+   try {
+    const textContentMD = await readFile(`./content/reviews/${slug}.md`, "utf-8");
+    const {
+      data: { title, date, image },
+      content,
+    } = matter(textContentMD);
+    const body = marked(content);
+    return { slug, title, date, image, body };
+  } catch (error) {
+    console.log("ERROR", error)
+    return { slug, title:` No review of "${slug}" title` , date:"no date", image:"no image", body:"No review exists" };
+  }
 }
 
 export async function getReviews(): Promise<Review[]> {
   const files = await readdir("./content/reviews");
   //filter and  strip put the file extension
-  const slugs =  await getSlugs();
+  const slugs = await getSlugs();
   console.log("[SLUGS", slugs);
   const reviews: Review[] = [];
   for (const slug of slugs) {
     const review = await getReview(slug);
     reviews.push(review);
   }
-  reviews.sort((a, b) => b.date.localeCompare(a.date))
+  reviews.sort((a, b) => b.date.localeCompare(a.date));
   return reviews;
 }
 
