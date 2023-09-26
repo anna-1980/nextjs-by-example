3a06fa614218c17e180651d2c099ddd8b1eeded8
Anna Zdyb <anna.zdyb@gmail.com>
2023-09-26 11:06:30 +0200

    testing tailwind classes

--- components/header-component/header-component.tsx
+++ components/header-component/header-component.tsx
@@ -5,12 +5,10 @@ export default function Header(): JSX.Element {
   return (
     <header>
       <div className="flex-row">
-        <div className="  ">
-          <NavComponent />
-          <h2 className="  bg-green-700 text-orange-50 px-2 hover:bg-sky-950 cursor-grab ">
-            Brand Name
-          </h2>
-        </div>
+        <NavComponent />
+        <h2 className="  bg-green-700 text-orange-50 px-2 hover:bg-sky-950 cursor-grab ">
+          Brand Name
+        </h2>
       </div>
     </header>
   );
